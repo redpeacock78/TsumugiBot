3cd03bdcb0165571b84abfbc44965b282dab526e
redpeacock78 <scarletpeacock78@gmail.com>
2022-07-09 20:10:15 +0900

    chore: Change src/actions/speakVoiceChannel.ts

--- src/actions/speakVoiceChannel.ts
+++ src/actions/speakVoiceChannel.ts
@@ -1,15 +1,20 @@
 import * as Discord from "discord.js";
 import * as DiscordVoice from "@discordjs/voice";
-import createVoice from "../libs/createVoice";
+import editMessage from "libs/editMessage";
+import createVoice from "libs/createVoice";
 
+/**
+ * Discordのボイスチャンネルで音声を再生
+ * @param {Discord.Client<boolean>} client
+ * @returns {void}
+ */
 export const speakVoiceChannel = (client: Discord.Client<boolean>): void => {
   client.on("messageCreate", async (message: Discord.Message<boolean>): Promise<void> => {
     const channel: Discord.VoiceBasedChannel = message.member?.voice
       .channel as Discord.VoiceBasedChannel;
     if (
       message.author.bot ||
-      message.content === "!tsumugijoin" ||
-      message.content === "!tsumugibye"
+      /^!(tsumugijoin|join|j|tsumugibye|bye|b)$/.test(message.content)
     )
       return;
     const connection: DiscordVoice.VoiceConnection = DiscordVoice.joinVoiceChannel({
@@ -22,7 +27,7 @@ export const speakVoiceChannel = (client: Discord.Client<boolean>): void => {
     const player: DiscordVoice.AudioPlayer = DiscordVoice.createAudioPlayer();
     connection.subscribe(player);
     await createVoice({
-      text: message.content,
+      text: editMessage(message.content),
       speaker: 8,
     })
       .then(async (stream): Promise<void> => {
