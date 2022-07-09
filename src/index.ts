0b74016a7719cf0e52d0e4ecbab8f3174e7c7a4a
redpeacock78 <scarletpeacock78@gmail.com>
2022-07-09 20:10:30 +0900

    chore: Change src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import * as Discord from "discord.js";
-import * as actions from "./actions";
-import * as commands from "./commands";
+import * as actions from "actions";
+import * as commands from "commands";
 
 const client: Discord.Client<boolean> = new Discord.Client({
   intents: Object.keys(Discord.Intents.FLAGS) as Discord.BitFieldResolvable<
